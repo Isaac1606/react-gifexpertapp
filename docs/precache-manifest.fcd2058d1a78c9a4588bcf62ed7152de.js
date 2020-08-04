a6e4d871b37f6efb2b8a59c7b0a8b70773b042ed
Isaac <isaac-dx@live.com.mx>
2020-08-04 18:33:16 -0500

    docs uploaded

---
+++ docs/precache-manifest.fcd2058d1a78c9a4588bcf62ed7152de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e45f7d51f1b76428e81bebcfeeae0b94",
+    "url": "/index.html"
+  },
+  {
+    "revision": "205d333c8a8532d6054f",
+    "url": "/static/css/main.3644cc05.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "205d333c8a8532d6054f",
+    "url": "/static/js/main.de938869.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
